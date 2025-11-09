0ac65da86b3c7d09287b49c08468a36a21043c0a
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2025-11-10 00:16:22 +0600

    Used client site and server site separately

---
+++ src/app/meals/page.jsx
@@ -0,0 +1,38 @@
+import MealSearchInput from "./components/MealSearchInput";
+
+const MealsPage = async ({ searchParams }) => {
+  const query = await searchParams;
+  const fetchMeals = async () => {
+    try {
+      const res = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${query.search}`
+      );
+      const data = await res.json();
+      //   setMeals(data?.meals || []);
+      return data.meals;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const meals = await fetchMeals();
+
+  return (
+    <>
+      <MealSearchInput></MealSearchInput>
+      <div className="grid grid-cols-4 gap-4">
+        {meals?.map((singleMeal) => {
+          return (
+            <div>
+              <p className="text-4xl font-bold">{singleMeal.strMeal}</p>
+              <p>{singleMeal.strInstructions}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default MealsPage;
